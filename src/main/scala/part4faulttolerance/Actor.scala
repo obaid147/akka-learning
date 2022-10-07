31d4630097accf31d87ac34db4c3f020b064eaf6
obaid147 <obaidfayazwani@gmail.com>
2022-10-07 12:22:54 +0530

    ActorInstance, ActorReferene, ActorPath

---
+++ src/main/scala/part4faulttolerance/Actor.scala
@@ -0,0 +1,19 @@
+package part4faulttolerance
+
+/** #1 ***** Actor Instance ***** Denoted by diamond.
+ * Has Methods.
+ * May have Internal state.
+ *
+ * #2 ***** Actor Reference (incarnation) ***** denoted by big circle with instance and mail-box inside.
+ * created with context/system.actorOf.
+ * has mailbox and can receive and process messages.
+ * contains one actor instance at any one time.
+ * contains UUID given by the actor system.
+ *
+ * #3 ***** Actor Path ***** denoted by yellow plane with actor reference inside.
+ * may or may not have actorRef inside.
+ * */
+
+class Actor {
+
+}
